0614b4e6867ac002998a77f68c35dcff7f25d279
Nitin Kumar <nitinkumar6e6b@gmail.com>
2025-05-27 00:41:53 +0530

    fixed search

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,20 +6,25 @@ function Header() {
       <h1 className="text-xl font-semibold text-gray-700">
         Healthcare Dashboard
       </h1>
-      <div className="relative w-1/3">
+
+      <div className="relative">
+        {/* Desktop search input */}
         <input
           type="text"
           placeholder="Search..."
-          className="w-full px-4 py-2 border-1 rounded-full focus:outline-none focus:ring-1 focus:ring-black-400"
+          className="hidden sm:block w-64 px-4 py-2 border rounded-full focus:outline-none focus:ring-1 focus:ring-black"
         />
-        <span className="absolute right-3 top-2.5 text-gray-400">🔍</span>
+        {/* Mobile search icon */}
+        <span className="sm:hidden text-2xl text-gray-500 cursor-pointer">
+          🔍
+        </span>
       </div>
 
-      <div className="flex items-center space-x-1">
+      <div className="flex items-center space-x-2">
         <span className="w-8 h-8 bg-blue-500 rounded-full flex items-center justify-center text-white font-bold">
           N
         </span>
-         <span className="w-8 h-8 bg-pink-500 rounded-full flex items-center justify-center text-white font-bold">
+        <span className="w-8 h-8 bg-pink-500 rounded-full flex items-center justify-center text-white font-bold">
           +
         </span>
       </div>

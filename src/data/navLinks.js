4a674c22cd5ca95053366c5f4cb7d8275f598afb
Nitin Kumar <avinashkumaralpha@gmail.com>
2025-09-13 14:50:19 +0530

    added medicine section

--- src/data/navLinks.js
+++ src/data/navLinks.js
@@ -1,9 +1,10 @@
 export const navLinks = [
-  { name: 'Dashboard', icon: '📊' },
-  { name: 'History', icon: '📜' },
-  { name: 'Calendar', icon: '📅' },
-  { name: 'Appointments', icon: '🗓️' },
-  { name: 'Statistics', icon: '📈' },
-  { name: 'Chat', icon: '💬' },
-  { name: 'Support', icon: '🛠️' },
+  { name: "Dashboard", icon: "📊" },
+  { name: "History", icon: "📜" },
+  { name: "Medicines", icon: "💉" },
+  { name: "Calendar", icon: "📅" },
+  { name: "Appointments", icon: "🗓️" },
+  { name: "Statistics", icon: "📈" },
+  { name: "Chat", icon: "💬" },
+  { name: "Support", icon: "🛠️" },
 ];
